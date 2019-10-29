d1fcce3d753d99c901d1715e756d42b0fc3df579
Mariam Charkviani <m.charkviani@adjarabet.com>
2019-10-29 18:41:19 +0400

    authentication, users and permissions

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -17,6 +17,8 @@ const advancedResults = require('../middleware/advancedResults');
 // Include other resource routers
 const courseRouter = require('./courses');
 
+const { protect, authorize } = require('../middleware/auth');
+
 // Re-route into other resource routers
 // გადაამისამართების კურსების ურლ-ზე
 router.use('/:bootcampId/courses', courseRouter);
@@ -26,14 +28,16 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const fileupload = require('express-fileupload');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -17,6 +18,9 @@ const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -38,10 +42,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
